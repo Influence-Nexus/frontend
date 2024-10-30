a161ee82a536208e4ab8fc076cc960948ee36d46
Shalunishe4ka <gt05dag@mail.ru>
2024-10-30 13:54:30 +0300

    decomposed solar. fixed animation

---
+++ frontend/src/components/SoralSystem/ModalWindowCards.jsx
@@ -0,0 +1,197 @@
+import CloseIcon from "@mui/icons-material/Cancel";
+import { cards, cardcreds } from "./cards";
+import { Link } from "react-router-dom";
+import { Carousel, Button, Modal } from "react-bootstrap";
+import "./styles.css"; // Import the CSS file for styling
+import { useState } from "react";
+
+export const PlanetCard = ({ selectedPlanet, setSelectedPlanet }) => {
+  const [showReviewWindow, setShowReviewWindow] = useState(false); // Состояние для окна "Исследуйте граф"
+  const [selectedCardIndex, setSelectedCardIndex] = useState(null); // Хранилище для индексов
+  const [isClosing, setIsClosing] = useState(false); // Для работы анимации
+  const [isZoomed, setIsZoomed] = useState(false); // State for zoom effect
+  // const [picked, setPicked] = useState(null);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const cardsPerPage = 6;
+
+  const handleCloseReviewWindow = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsClosing(false);
+      setShowReviewWindow(false);
+    }, 700);
+  };
+
+  const handleOpenReviewWindow = (index) => {
+    setIsZoomed(true); // Set zoom state
+    setTimeout(() => {
+      setShowReviewWindow(true);
+      setSelectedCardIndex(index);
+      // setPicked(index);
+    }, 700); // Delay to allow zoom animation to complete
+  };
+
+  const handleZoomOut = () => {
+    setIsZoomed(false);
+    setIsClosing(false);
+    setTimeout(() => {
+      setSelectedCardIndex(null);
+    }, 700);
+  };
+
+  const handleCardClick = (event) => {
+    if (isZoomed) {
+      event.stopPropagation(); // Prevent the click from propagating to the parent
+    }
+  };
+
+  // const indexOfLastCard = currentPage * cardsPerPage;
+  // const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  // const currentCards = cards[selectedPlanet.name].slice(
+  //   indexOfFirstCard,
+  //   indexOfLastCard
+  // );
+  const currentCards = cards[selectedPlanet.name];
+
+  return (
+    <div className="planet-cardcard text-white bg-dark mb-3">
+      <div className="segment-domen">
+        <div className="segment-domen-header">
+          <div className="segment-domen-planet">
+            <img
+              className="planet-image"
+              src={cardcreds[selectedPlanet.name].src}
+            />
+            <div>
+              <h1
+                className="planet-name"
+                style={{
+                  fontSize: "80px",
+                  color: cardcreds[selectedPlanet.name].color,
+                }}
+              >
+                {cardcreds[selectedPlanet.name].name}
+              </h1>
+              <h3>
+                <span style={{ color: cardcreds[selectedPlanet.name].color }}>
+                  Стратегия жизни:
+                </span>{" "}
+                {cardcreds[selectedPlanet.name].desc}
+              </h3>
+            </div>
+          </div>
+          <Link
+            to="/"
+            className="return-main"
+            style={{
+              color: cardcreds[selectedPlanet.name].color,
+              borderColor: cardcreds[selectedPlanet.name].color,
+            }}
+          >
+            Main
+          </Link>
+        </div>
+
+        <div className="segment-cards">
+          {currentCards.map((segment, index) => (
+            <div
+              key={segment.index}
+              className={`card text-white bg-secondary mb-3 segment-card segment-card-${index} ${
+                isZoomed && index === selectedCardIndex ? "zoomed" : "unzoomed"
+              }`}
+              onClick={
+                isZoomed && index === selectedCardIndex
+                  ? handleZoomOut
+                  : () => {}
+              }
+            >
+              <div className="card-header">
+                <p>{segment.title}</p>
+              </div>
+              <div className="card-body" onClick={handleCardClick}>
+                <img
+                  width={isZoomed && index === selectedCardIndex ? "80%" : 160}
+                  height={isZoomed && index === selectedCardIndex ? 350 : 160}
+                  src={segment.image}
+                  alt={segment.title}
+                />
+                {isZoomed && index === selectedCardIndex && (
+                  <div className="card-description">
+                    <p>{segment.description}</p>
+                  </div>
+                )}
+                <div className="text-center">
+                  {isZoomed && index === selectedCardIndex ? (
+                    <button
+                      className="btn-CLOSE"
+                      style={{
+                        color: cardcreds[selectedPlanet.name].color,
+                        borderColor: cardcreds[selectedPlanet.name].color,
+                        backgroundColor: "transparent",
+                        borderRadius: "10px",
+                      }}
+                      onClick={handleZoomOut}
+                    >
+                      Close
+                    </button>
+                  ) : (
+                    <button
+                      className="btn-CHECK"
+                      style={{
+                        color: cardcreds[selectedPlanet.name].color,
+                        borderColor: cardcreds[selectedPlanet.name].color,
+                      }}
+                      onClick={() => handleOpenReviewWindow(index)} // Open modal on button click
+                    >
+                      pick
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* Добавляем кнопку для возврата к виду солнечной системы */}
+        <Button
+          variant="secondary"
+          onClick={() => setSelectedPlanet(null)}
+          className="mb-3"
+          size="sm"
+          style={{
+            width: "55px",
+            position: "absolute",
+            top: "90%",
+            left: "95%",
+          }}
+        >
+          <CloseIcon />
+        </Button>
+        {/* Окно "Исследуйте граф" */}
+        <Modal
+          show={showReviewWindow}
+          centered
+          animation={false}
+          className={`modal-window ${isClosing ? "in" : "out"}`}
+          dialogClassName="custom-modal"
+          contentClassName="custom-modal-content"
+        >
+          <Modal.Body className="GraphReviewModalBody">
+            <Modal.Title>Graph Preview</Modal.Title>
+          </Modal.Body>
+          <Modal.Footer className="GraphReviewModalFooter">
+            <Link
+              id="buttonOkGraphReview"
+              to={`/matrix/${selectedCardIndex + 1}`}
+            >
+              <p>Ok</p>
+            </Link>
+            <button id="buttonNoGraphReview" onClick={handleCloseReviewWindow}>
+              <p>Next</p>
+            </button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/components/SoralSystem/cards.js
@@ -0,0 +1,237 @@
+export const cards = {
+  "Blue-Green": [
+    {
+      index: 0,
+      title: "Связывание углерода",
+      description: "Связывание углерода",
+      link: "https://search.app/VePfKaJAwjYRozsM7",
+      image:
+        "https://pics.craiyon.com/2023-09-23/cd587327ae3648c493067e63fbe16932.webp",
+    },
+    {
+      index: 1,
+      title: "Глобальное потепление",
+      description: "Глобальное потепление",
+      link: "https://search.app/xXaWBqBmVUwJikXc9",
+      image:
+        "https://pics.craiyon.com/2023-09-16/2cb8e418aa87473aafb8a8c56aa0e773.webp",
+    },
+    {
+      index: 2,
+      title: "Качество и доступность среды обитания",
+      description: "Качество и доступность среды обитания",
+      link: "https://search.app/2aWfsW4YhZRGbd6UA",
+      image:
+        "https://pics.craiyon.com/2023-09-15/cd5c90837994479fbd580f5b7d64240c.webp",
+    },
+    {
+      index: 3,
+      title: "Коммерческое рыболовство",
+      description: "Коммерческое рыболовство",
+      link: "https://search.app/CWyYe4kaJtaShWyR8",
+      image: "https://pics.craiyon.com/2023-11-10/ll7zHv8dSI2e0XDXjBW2xQ.webp",
+    },
+    {
+      index: 4,
+      title: "Спортивная рыбалка",
+      description: "Спортивная рыбалка",
+      link: "https://www.craiyon.com/image/2h8pJ0zuQD6mawpHOjH3zw",
+      image:
+        "https://pics.craiyon.com/2023-10-07/36ff413db3da42068dd0d4a34f660fb6.webp",
+    },
+    {
+      index: 5,
+      title: "Прогулочное катание на лодках",
+      description: "Прогулочное катание на лодках",
+      link: "https://www.craiyon.com/image/lMkDXUSNTuuwRuBOoFBeug",
+      image:
+        "https://pics.craiyon.com/2023-09-26/fd5b6d13339241f1be0409f19486cedb.webp",
+    },
+    {
+      index: 6,
+      title: "Производство дикого риса",
+      description: "Производство дикого риса",
+      link: "https://search.app/5ANeLspNrfGqRovKA",
+      image:
+        "https://pics.craiyon.com/2023-11-02/7284eedd54c446ea8137ca4e1557f4ec.webp",
+    },
+    {
+      index: 7,
+      title: "Урожайность озимой пшеницы",
+      description: "Урожайность озимой пшеницы",
+      link: "https://www.craiyon.com/image/-bcetbHzQheAf0vSOaLmpQ",
+      image: "https://pics.craiyon.com/2023-11-26/nKXjW6YZTpq6tQLPENp29g.webp",
+    },
+    {
+      index: 8,
+      title: "Риск лесного пожара",
+      description: "Риск лесного пожара",
+      link: "",
+      image: "/textures/wildfire.png",
+    },
+    {
+      index: 9,
+      title: "Деградация среды обитания",
+      description:
+        "Деградация среды обитания за счет вырубки лесов в сельской горной местности",
+      link: "https://www.craiyon.com/image/riRK9KyZREqUyhEEWcf_UQ",
+      image:
+        "https://pics.craiyon.com/2023-09-26/0ba6de42a92742eca6765d15c71337d4.webp",
+    },
+  ],
+  Violet: [
+    {
+      index: 0,
+      title:
+        "Средства к существованию на основе разведения крупного рогатого скота",
+      description:
+        "Средства к существованию на основе разведения крупного рогатого скота",
+      link: "https://search.app/v3gP3M1KYo56szXT8",
+      image: "IMAGE_LINK_11",
+    },
+    {
+      index: 1,
+      title:
+        "Средства к существованию, основанные на растениеводстве и мелком скотоводстве",
+      description:
+        "Средства к существованию, основанные на растениеводстве и мелком скотоводстве",
+      link: "https://search.app/1tYx1f76uoSNkcho9",
+      image: "IMAGE_LINK_12",
+    },
+    {
+      index: 2,
+      title: "Средства к существованию, не связанные с сельским хозяйством",
+      description: "Нефермерские средства к существованию в сельской местности",
+      link: "https://search.app/SeUHtX8h8zrJR51r5",
+      image: "IMAGE_LINK_13",
+    },
+    {
+      index: 3,
+      title: "Стабильность предприятия",
+      description: "Стабильность предприятия",
+      link: "https://search.app/S1XZLAQugrctiDJe9",
+      image: "IMAGE_LINK_14",
+    },
+    {
+      index: 4,
+      title: "Надежность и безопасность автомобиля",
+      description: "Надежность и безопасность автомобиля",
+      link: "https://www.craiyon.com/image/B5R28A_3RZKJTn4WByAAvw",
+      image: "IMAGE_LINK_15",
+    },
+    {
+      index: 5,
+      title: "Доля рынка",
+      description: "Увеличение доли рынка компании за счет диверсификации",
+      link: "https://search.app/1adA5qcaNaSWsvBHA",
+      image: "IMAGE_LINK_16",
+    },
+    {
+      index: 6,
+      title: "Знания персонала",
+      description: "Знания персонала",
+      link: "https://search.app/b6cvsj1J5bs7hnGw6",
+      image: "IMAGE_LINK_17",
+    },
+    {
+      index: 7,
+      title: "Производственные затраты",
+      description: "Производственные затраты",
+      link: "https://www.craiyon.com/image/SqgBjmGdRJClAq5mhav3bA",
+      image: "IMAGE_LINK_18",
+    },
+    {
+      index: 8,
+      title: "Клинический риск применения лекарственных препаратов",
+      description: "Клинический риск в администрировании лекарств",
+      link: "https://www.craiyon.com/image/2U4cGPxeTn-tqFstIIAztA",
+      image: "IMAGE_LINK_19",
+    },
+    {
+      index: 9,
+      title: "Выбор научной конференции",
+      description: "Выбор научной конференции",
+      link: "https://www.craiyon.com/image/5sG-Mkv2QVW-zAe2TRZblA",
+      image: "IMAGE_LINK_20",
+    },
+    {
+      index: 10,
+      title: "Монтаж трубопроводов",
+      description: "Монтаж трубопроводов",
+      link: "https://www.craiyon.com/image/ATspx3rPR-ibVF982ERPsQ",
+      image: "IMAGE_LINK_21",
+    },
+    {
+      index: 11,
+      title: "Энергетический переход",
+      description: "Энергетический переход",
+      link: "https://search.app/mkg6Ccuhs2v9xcqy8",
+      image: "IMAGE_LINK_22",
+    },
+  ],
+  Orange: [
+    {
+      index: 0,
+      title: "Преступление и наказание",
+      description: "Преступление и наказание",
+      link: "https://www.craiyon.com/image/f2TkYJ46TqKyyl0Rt_GJ3g",
+      image: "IMAGE_LINK_23",
+    },
+    {
+      index: 1,
+      title: "Кубинский ракетный кризис",
+      description: "Кубинский ракетный кризис",
+      link: "https://search.app/Yjx1o9gNj5d2jzaZ7",
+      image: "IMAGE_LINK_24",
+    },
+    {
+      index: 2,
+      title: "Финансовый кризис в Малайзии",
+      description: "Финансовый кризис спекулятивных инвестиций",
+      link: "https://search.app/Wy8Y3erT3fLKqgEB6",
+      image: "IMAGE_LINK_25",
+    },
+    {
+      index: 3,
+      title: "Финансовый кризис в Южной Кореи",
+      description: "Финансовый кризис промышленного развития",
+      link: "https://search.app/Lf1xh2KB8mMmD1tT8",
+      image: "IMAGE_LINK_26",
+    },
+    {
+      index: 4,
+      title: "Влияние ИТ на расходы по экономическому развитию",
+      description: "Влияние ИТ на расходы по экономическому развитию",
+      link: "https://www.craiyon.com/image/k_54MUGrRfCyrtE5GLuzJA",
+      image: "IMAGE_LINK_27",
+    },
+    {
+      index: 5,
+      title: "Бездомность",
+      description: "Бездомность",
+      link: "https://www.craiyon.com/image/OUFGuuZ8RNuZ0gh-BvYh_Q",
+      image: "IMAGE_LINK_28",
+    },
+  ],
+};
+
+export const cardcreds = {
+  "Blue-Green": {
+    name: "Blue-Green",
+    desc: "Жизнь в окружении природы / Сбережение природы / Качество среды обитания.",
+    src: "/imgs/green.jpg",
+    color: "#52ffbd",
+  },
+  Orange: {
+    name: "Orange",
+    desc: "Совершенное общественное устройство / Баланс социальных факторов / Настройка институционального комплекса.",
+    src: "/imgs/orange.jpg",
+    color: "#ff8b2b",
+  },
+  Violet: {
+    name: "Violet",
+    desc: "Устойчивое жизнеобеспечение / Сбережение природы / Надежность и безопасность.",
+    src: "/imgs/viol.png",
+    color: "#eea8ff",
+  },
+};
